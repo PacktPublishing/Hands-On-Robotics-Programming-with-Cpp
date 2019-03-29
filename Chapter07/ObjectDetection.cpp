b7d14a8cbbe718c47d4395d225c4396240b0e165
DineshT106 <dins106@gmail.com>
2019-03-29 11:38:26 +0530

    Add files via upload

--- Chapter07/ObjectDetection.cpp
+++ Chapter07/ObjectDetection.cpp
@@ -1,36 +1,30 @@
 #include <iostream>
-#include <opencv2/opencv.hpp>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-
+#include<opencv2/opencv.hpp>
+#include<opencv2/core/core.hpp>
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
 using namespace cv;
 using namespace std;
-
 int main()
 {
-
- Mat img, resizeimg,thresimage;
- img = imread("greenball.png");
- imshow("Green Ball Image", img);
- waitKey(0);
- 
- resize(img, resizeimg, cvSize(640, 480));
- imshow("Resized Image"resizeimg);
- waitKey(0);
-
- inRange(grayimage, Scalar(39, 140, 34), Scalar(122, 245, 119), thresimage);
- imshow("Thresholded Image", thresimage);
- waitKey(0);
- 
- Moments m = moments(thresimage,true);
- int x,y;
- x = m.m10/m.m00;
- y = m.m01/m.m00;
- Point p(x,y);
- circle(img, p, 5, Scalar(0,0,200), -1);
- imshow("Image with center",img);
- waitKey(0);
-
- return 0;
+Mat img, resizeimg,thresimage;
+img = imread("greenball.png");
+imshow("Green Ball Image", img);
+waitKey(0);
+resize(img, resizeimg, cvSize(640, 480));
+imshow("Resized Image", resizeimg);
+waitKey(0);
+inRange(resizeimg, Scalar(39, 140, 34), Scalar(122, 245, 119),
+thresimage);
+imshow("Thresholded Image", thresimage);
+waitKey(0);
+Moments m = moments(thresimage,true);
+int x,y;
+x = m.m10/m.m00;
+y = m.m01/m.m00;
+Point p(x,y);
+circle(img, p, 5, Scalar(0,0,200), -1);
+imshow("Image with center",img);
+waitKey(0);
+return 0;
 }
