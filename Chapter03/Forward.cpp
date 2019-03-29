01deb4175dacceaa644c4f8d59ed9d1d5c398bde
DineshT106 <dins106@gmail.com>
2019-03-29 09:15:30 +0530

    Add files via upload

--- Chapter03/Forward.cpp
+++ Chapter03/Forward.cpp
@@ -5,17 +5,17 @@ int main(void)
 {
 wiringPiSetup();
 pinMode(0,OUTPUT); 
-pinMode(1,OUTPUT); 
-pinMode(2,OUTPUT);
-pinMode(3,OUTPUT); 
+pinMode(2,OUTPUT); 
+pinMode(3,OUTPUT);
+pinMode(4,OUTPUT); 
  
  
  for(int i=0; i<1;i++)
  {
-digitalWrite(0,HIGH); //PIN O & 1 will move the Left Motor
-digitalWrite(1,LOW);
-digitalWrite(2,HIGH); //PIN 2 & 3 will move the Right Motor
-digitalWrite(3,LOW);
+digitalWrite(0,HIGH); //PIN O & 2 will move the Left Motor
+digitalWrite(2,LOW);
+digitalWrite(3,HIGH); //PIN 3 & 4 will move the Right Motor
+digitalWrite(4,LOW);
 delay(3000);
  }
 return 0;
--- Chapter03/RobotMovement.cpp
+++ Chapter03/RobotMovement.cpp
@@ -5,67 +5,71 @@ int main(void)
 {
 wiringPiSetup();
 pinMode(0,OUTPUT); 
-pinMode(1,OUTPUT); 
-pinMode(2,OUTPUT);
-pinMode(3,OUTPUT); 
+pinMode(2,OUTPUT); 
+pinMode(3,OUTPUT);
+pinMode(4,OUTPUT); 
  
  
  for(int i=0; i<1;i++)
  {
 	 
 /* FORWARD */
-digitalWrite(0,HIGH); //PIN O & 1 will move the Left Motor Forward
-digitalWrite(1,LOW);
-digitalWrite(2,HIGH); //PIN 2 & 3 will move the Right Motor Forward
-digitalWrite(3,LOW);
+digitalWrite(0,HIGH); //PIN O & 2 will move the Left Motor Forward
+digitalWrite(2,LOW);
+digitalWrite(3,HIGH); //PIN 3 & 4 will move the Right Motor Forward
+digitalWrite(4,LOW);
 delay(3000);
 
 /* BACKWARD */
-digitalWrite(0,LOW); //PIN O & 1 will move the Left Motor Backward
-digitalWrite(1,HIGH);
-digitalWrite(2,LOW); //PIN 2 & 3 will move the Right Motor Backward
-digitalWrite(3,HIGH);
+digitalWrite(0,LOW); 
+digitalWrite(2,HIGH);
+digitalWrite(3,LOW); 
+digitalWrite(4,HIGH);
 delay(3000);
 
 /* STOP */
-digitalWrite(0,HIGH); //PIN O & 1 will stop the Left Motor
-digitalWrite(1,HIGH);
-digitalWrite(2,HIGH); //PIN 2 & 3 will stop the Right Motor
-digitalWrite(3,HIGH);
+digitalWrite(0,HIGH); 
+digitalWrite(2,HIGH);
+digitalWrite(3,HIGH); 
+digitalWrite(4,HIGH);
 delay(3000);
 
 /* AXIAL LEFT TURN */
 digitalWrite(0,LOW);
-digitalWrite(1,HIGH);
 digitalWrite(2,HIGH);
-digitalWrite(3,LOW);
+digitalWrite(3,HIGH);
+digitalWrite(4,LOW);
 delay(500);
 
 /* AXIAL RIGHT TURN */
 digitalWrite(0,HIGH);
-digitalWrite(1,LOW);
 digitalWrite(2,LOW);
-digitalWrite(3,HIGH);
+digitalWrite(3,LOW);
+digitalWrite(4,HIGH);
 delay(500);
 
 
 /* RADIAL LEFT TURN */
 digitalWrite(0,HIGH);
-digitalWrite(1,HIGH);
 digitalWrite(2,HIGH);
-digitalWrite(3,LOW);
+digitalWrite(3,HIGH);
+digitalWrite(4,LOW);
 delay(1000);
 
 
 /* RADIAL RIGHT TURN */
 digitalWrite(0,HIGH);
-digitalWrite(1,LOW);
-digitalWrite(2,HIGH);
+digitalWrite(2,LOW);
 digitalWrite(3,HIGH);
+digitalWrite(4,HIGH);
 delay(1000);
 
-
-
+/* STOP */
+digitalWrite(0,HIGH);
+digitalWrite(2,HIGH);
+digitalWrite(3,HIGH);
+digitalWrite(4,HIGH);
+delay(1000);
  }
 return 0;
  }
